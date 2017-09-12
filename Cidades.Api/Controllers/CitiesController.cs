31d5a08266b21a8fae16b2b5b309eee5323aba6d
Fabio Galante Mans <fabio.mans@superdigital.com.br>
2017-09-12 13:46:46 -0300

    Cities

---
+++ Cidades.Api/Controllers/CitiesController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Cidades.Api.Controllers
+{
+    //[Produces("application/json")]
+    [Route("api/Cities")]
+    public class CitiesController : Controller
+    {
+        public JsonResult GetCities()
+        {
+            return new JsonResult(new List<object>
+            {
+                new {id = 1, Name = "São Paulo"},
+                new {id = 2, Name = "Rio de Janeiro"}
+            });
+        }
+
+
+        //public JsonResult GetCities2() => new JsonResult(new List<object>
+        //{
+        //    new {id = 1, Name = "São Paulo"},
+        //    new {id = 2, Name = "Rio de Janeiro"}
+        //});
+    }
+}
--- Cidades.Api/Startup.cs
+++ Cidades.Api/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Formatters;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Cidades.Api
@@ -15,6 +16,9 @@ namespace Cidades.Api
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc()
+                .AddMvcOptions(o => o.OutputFormatters.Add(
+                    new XmlDataContractSerializerOutputFormatter()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,11 +28,24 @@ namespace Cidades.Api
             {
                 app.UseDeveloperExceptionPage();
             }
-
-            app.Run(async (context) =>
+            else
             {
-                await context.Response.WriteAsync("Hello World!");
-            });
+                app.UseExceptionHandler();
+            }
+
+            app.UseMvc();
+
+            //app.Run(context =>
+            //{
+            //    throw new Exception("Example exception");
+            //});
+
+            
+
+            //app.Run(async (context) =>
+            //{
+            //    await context.Response.WriteAsync("Hello World!");
+            //});
         }
     }
 }
